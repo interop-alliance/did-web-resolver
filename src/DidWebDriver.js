94ac870cd5be3bf4530f64ac7848db9776bf1f69
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-05-08 11:27:47 -0400

    Implement didFromUrl and urlFromDid, add tests.

--- src/DidWebDriver.js
+++ src/DidWebDriver.js
@@ -1,5 +1,119 @@
 'use strict'
 
 // import axios from 'axios' // todo: consider 'apisauce' instead
+// import { DidDocument } from 'did-io'
+import { URL } from 'whatwg-url'
 
-export class DidWebDriver {}
+const DEFAULT_KEY_TYPE = 'Ed25519VerificationKey2018'
+const DEFAULT_SUPPORTED_KEY_TYPES = [
+  'RsaVerificationKey2018', 'Ed25519VerificationKey2018'
+]
+
+export function didFromUrl ({ url } = {}) {
+  if(!url) {
+    throw new TypeError('Cannot convert url to did, missing url.')
+  }
+  if(url.startsWith('http:')) {
+    throw new TypeError('did:web does not support non-HTTPS URLs.')
+  }
+
+  let parsedUrl
+  try {
+    parsedUrl = new URL(url)
+  } catch (error) {
+    throw new TypeError(`Invalid url: "${url}".`)
+  }
+
+  const { host, pathname } = parsedUrl
+
+  let pathComponent = ''
+  if (pathname && pathname !== '/' && pathname !== '/.well-known/did.json') {
+    pathComponent = pathname.split('/').map(encodeURIComponent).join(':')
+  }
+
+  return 'did:web:' + encodeURIComponent(host) + pathComponent
+}
+
+export function urlFromDid ({ did } = {}) {
+  if(!did) {
+    throw new TypeError('Cannot convert did to url, missing did.')
+  }
+  if(!did.startsWith('did:web:')) {
+    throw new TypeError(`DID Method not supported: "${did}".`)
+  }
+
+  const [_did, _web, host, ...pathFragments ] = did.split(':')
+
+  let pathname = ''
+  if(pathFragments.length === 0) {
+    pathname = '/.well-known/did.json'
+  } else {
+    pathname = '/' + pathFragments.map(decodeURIComponent).join('/')
+  }
+
+  return 'https://' + decodeURIComponent(host) + pathname
+}
+
+export class DidWebDriver {
+  constructor ({ supportedKeyTypes = DEFAULT_SUPPORTED_KEY_TYPES } = {}) {
+    // did:web:...
+    this.method = 'web'
+    this.supportedKeyTypes = supportedKeyTypes
+  }
+
+  /**
+   * Generates and returns the id of a given key. Used by `did-io` drivers.
+   *
+   * @param {LDKeyPair} key
+   * @param {string} did
+   *
+   * @returns {Promise<string>} Returns the key's id.
+   */
+  async computeKeyId ({ key, did }) {
+    return `${did}#${key.fingerprint()}`
+  }
+
+  /**
+   * Generates a new DID Document and initializes various authentication
+   * and authorization proof purpose keys.
+   *
+   * Usage:
+   * ```
+   *   const url = 'https://example.com'
+   *   const {didDocument, keys} = await didWeb.generate({url})
+   *   didDocument.id
+   *   // -> 'did:web:example.com'
+   * ```
+   *
+   * Either an `id` or a `url` is required:
+   * @param [id] {string} - A did:web DID. If absent, will be converted from url
+   * @param [url] {string}
+   *
+   * @param [keys={}] {object} Map of keys to be generated, by key purpose.
+   *
+   * @param [keyType] {string} If existing keys are not passed in, default
+   *   keys are generated using this key type (defaults to
+   *   'Ed25519VerificationKey2018').
+   *
+   * @throws {Error}
+   *
+   * @returns {Promise<{didDocument: DidDocument, didKeys: object}>}
+   */
+  async generate ({ id, url, keys = {}, keyType = DEFAULT_KEY_TYPE } = {}) {
+    if (!this.supportedKeyTypes.includes(keyType)) {
+      throw new Error(`Unknown key type: "${keyType}".`)
+    }
+
+    const didDocument = new DidDocument({ id: id || didFromUrl({ url }) })
+    const { didKeys } = await didDocument.initKeys({ keys, keyType })
+    return { didDocument, didKeys }
+  }
+
+  /**
+   * Fetches a DID Document for a given DID.
+   * @param {string} did
+   *
+   * @returns {Promise<DidDocument>}
+   */
+  async get () {}
+}
--- test/unit/DidWebDriver.spec.js
+++ test/unit/DidWebDriver.spec.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { DidWebDriver } from '../../src'
+import { DidWebDriver, urlFromDid, didFromUrl } from '../../src'
 
 import chai from 'chai'
 import dirtyChai from 'dirty-chai'
@@ -12,6 +12,108 @@ describe('DidWebDriver', () => {
   describe('constructor', () => {
     it('should exist', () => {
       expect(new DidWebDriver()).to.exist()
+    })
+  })
+
+  describe('urlFromDid', () => {
+    it('should error on missing did', () => {
+      let error
+      try {
+        urlFromDid()
+      } catch (e) {
+        error = e
+      }
+      expect(error.message).to.equal('Cannot convert did to url, missing did.')
+    })
+
+    it('should error on non-did:web dids', () => {
+      let error
+      try {
+        urlFromDid({ did: 'did:example:1234' })
+      } catch (e) {
+        error = e
+      }
+      expect(error.message)
+        .to.equal('DID Method not supported: "did:example:1234".')
+    })
+
+    it('should convert first id fragment to pathname plus default path', () => {
+      expect(urlFromDid({ did: 'did:web:example.com' }))
+        .to.equal('https://example.com/.well-known/did.json')
+    })
+
+    it('should url-decode host', () => {
+      expect(urlFromDid({ did: 'did:web:localhost%3A8080' }))
+        .to.equal('https://localhost:8080/.well-known/did.json')
+    })
+
+    it('should url-decode path fragments', () => {
+      expect(urlFromDid({ did: 'did:web:example.com:path:some%2Bsubpath' }))
+        .to.equal('https://example.com/path/some+subpath')
+    })
+  })
+
+  describe('didFromUrl', () => {
+    it('should error on missing url', () => {
+      let error
+      try {
+        didFromUrl()
+      } catch (e) {
+        error = e
+      }
+      expect(error.message).to.equal('Cannot convert url to did, missing url.')
+    })
+
+    it('should error on http URLs', () => {
+      let error
+      try {
+        didFromUrl({ url: 'http://example.com' })
+      } catch (e) {
+        error = e
+      }
+      expect(error.message).to.equal('did:web does not support non-HTTPS URLs.')
+    })
+
+    it('should error on invalid URLs', () => {
+      let error
+      try {
+        didFromUrl({ url: 'non-url' })
+      } catch (e) {
+        error = e
+      }
+      expect(error.message).to.equal('Invalid url: "non-url".')
+    })
+
+    it('should convert host to did identifier', () => {
+      expect(didFromUrl({ url: 'https://localhost' }))
+        .to.equal('did:web:localhost')
+      expect(didFromUrl({ url: 'https://example.com' }))
+        .to.equal('did:web:example.com')
+    })
+
+    it('should url-encode host', () => {
+      expect(didFromUrl({ url: 'https://localhost:8080' }))
+        .to.equal('did:web:localhost%3A8080')
+    })
+
+    it('should leave off the default / path', () => {
+      expect(didFromUrl({ url: 'https://example.com/' }))
+        .to.equal('did:web:example.com')
+    })
+
+    it('should encode path / separators as :', () => {
+      expect(didFromUrl({ url: 'https://example.com/path/subpath/did.json' }))
+        .to.equal('did:web:example.com:path:subpath:did.json')
+    })
+
+    it('should drop the default /.well-known/did.json pathname', () => {
+      expect(didFromUrl({ url: 'https://example.com/.well-known/did.json' }))
+        .to.equal('did:web:example.com')
+    })
+
+    it('should url-encode path fragments', () => {
+      expect(didFromUrl({ url: 'https://example.com/path/some+subpath' }))
+        .to.equal('did:web:example.com:path:some%2Bsubpath')
     })
   })
 })
